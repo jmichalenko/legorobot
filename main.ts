8c45e9ed908aa97a9d10484a2d24c4cdb0a94206
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2023-05-05 16:45:05 -0400

    v2

--- main.ts
+++ main.ts
@@ -1,38 +1,33 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if ((0 as any) == (1 as any)) {
+    if (receivedNumber == 1) {
         pins.digitalWritePin(DigitalPin.P1, 1)
         pins.digitalWritePin(DigitalPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P12, 1)
         basic.showString("Forward")
-    } else if (false) {
-        if (receivedNumber == 2) {
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            pins.digitalWritePin(DigitalPin.P2, 1)
-            pins.digitalWritePin(DigitalPin.P8, 0)
-            pins.digitalWritePin(DigitalPin.P12, 1)
-            basic.showString("Backwards")
-        }
+    } else if (receivedNumber == 2) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        basic.showString("Backwards")
     } else if (receivedNumber == 3) {
         pins.digitalWritePin(DigitalPin.P1, 1)
         pins.digitalWritePin(DigitalPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P12, 0)
         basic.showString("Left")
+    } else if (receivedNumber == 4) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showString("Right")
     } else {
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 0)
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P12, 0)
     }
-})
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.AB, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
 })
 radio.setGroup(44)
